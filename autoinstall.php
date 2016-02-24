cf6484b3f914c35296627d8fe9bb4454f82ef939
hostellerie <hostellerie.org@gmail.com>
2016-02-24 15:58:06 +0100

    French language utf-8 without BOM

--- autoinstall.php
+++ autoinstall.php
@@ -51,8 +51,8 @@ function plugin_autoinstall_contact($pi_name)
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '1.2.0',
-        'pi_gl_version'   => '1.8.0',
+        'pi_version'      => '1.2.1',
+        'pi_gl_version'   => '2.0.0',
         'pi_homepage'     => 'http://geeklog.fr'
     );
 
